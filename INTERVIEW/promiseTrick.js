f20e45b33060cc5fed3a88e211b7ca353b927182
Vishal Sharma <vsvishal11@yahoo.com>
2023-12-30 19:02:26 +0530

    promise added

---
+++ INTERVIEW/promiseTrick.js
@@ -0,0 +1,34 @@
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise1 value resolved");
+  }, 5000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise2 value resolved");
+  }, 2000);
+});
+
+async function handlePromise() {
+  console.log("Hello World!!");
+
+  const val = await p1;
+  console.log("Namaster Vishal 1 🤠");
+  console.log(val);
+
+  const va2 = await p2;
+  console.log("Namaster Vishal 2 🤡");
+  console.log(va2);
+}
+
+// handlePromise();
+
+function getData() {
+  p1.then((res) => console.log(res, " 1 🐶"));
+  p2.then((res) => console.log(res, " 2 🦊"));
+
+  console.log("Hey LINE 17");
+}
+
+getData();
