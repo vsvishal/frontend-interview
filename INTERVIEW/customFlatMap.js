321b2400a64b75931dadb3e2865eab34640c0aaa
Vishal Sharma <vsvishal11@yahoo.com>
2024-01-02 17:43:06 +0530

    updated

--- INTERVIEW/customFlatMap.js
+++ INTERVIEW/customFlatMap.js
@@ -1,3 +1,19 @@
+// solution 1
+function customFlat1(arr, dept, res = [], curr = 0) {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) && curr < dept) {
+      customFlat1(arr[i], dept, res, curr + 1);
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+}
+
+// const arr = [[1, 2, [3, 3], 4], 6, 7, [8, 9]];
+console.log(customFlat1(arr, 2));
+
+// solution 2
 function customFlat(arr, dept = 1) {
   let result = [];
   arr.forEach((ar) => {
